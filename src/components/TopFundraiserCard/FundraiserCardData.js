eb31d0bc67ccd5c0cae7b5d06540be9e80673202
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-15 12:51:42 +0545

    image issue fixed

--- src/components/TopFundraiserCard/FundraiserCardData.js
+++ src/components/TopFundraiserCard/FundraiserCardData.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-export const FundraiserCardData=[
-  {
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  },
-  {
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  }
-  ,{
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  },
-  {
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  }
-]
-=======
 export const FundraiserCardData = [
 	{
 		cardimg:
@@ -63,4 +27,3 @@ export const FundraiserCardData = [
 		totalDonation: 'Rs 10,000 raised of Rs 120,000',
 	},
 ];
->>>>>>> 67e3816dc676d56936c1c40740679aacf15ba40a
