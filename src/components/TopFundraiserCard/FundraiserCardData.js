77fdf3bf538b7a3d3653b86355bdb7d005e14557
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-15 12:07:30 +0545

    fundraiser card

---
+++ src/components/TopFundraiserCard/FundraiserCardData.js
@@ -0,0 +1,26 @@
+export const FundraiserCardData=[
+  {
+    cardimg:require("../../assets/images/patient.jpg"),
+    location:'Biratnagar,Nepal',
+    title:'Help Saya Shakya Family',
+    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+    lastDonated:'Last donated 10min Ago',
+    totalDonation:'Rs 10,000 raised of Rs 120,000'
+  },
+  {
+    cardimg:require("../../assets/images/patient.jpg"),
+    location:'Biratnagar,Nepal',
+    title:'Help Saya Shakya Family',
+    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+    lastDonated:'Last donated 10min Ago',
+    totalDonation:'Rs 10,000 raised of Rs 120,000'
+  }
+  ,{
+    cardimg:require("../../assets/images/patient.jpg"),
+    location:'Biratnagar,Nepal',
+    title:'Help Saya Shakya Family',
+    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+    lastDonated:'Last donated 10min Ago',
+    totalDonation:'Rs 10,000 raised of Rs 120,000'
+  }
+]
---
+++ src/components/TopFundraiserCard/TopFundraiserCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {FundraiserCardData as data} from './FundraiserCardData'
+
+const TopFundraiserCard = () => {
+  const cardStyle={
+    width: '18rem'
+  }
+  const cardLineStyle={
+    height:'5px',
+    borderRadius:'8px',
+    color:'#9F1718'
+  }
+  return (
+    <div className="cardContainer">
+      <div className="row">
+        {data.map((data,key)=>{
+          return(
+            <div className="col-md-4" key={key}>
+            <div class="card" style={cardStyle}>
+              <img src={data.cardimg} class="card-img-top" alt="cardimg"/>
+              <div class="card-body">
+                <h5 class="card-title" style={{color:'#9F1718'}}>{data.location}</h5>
+                <h5>{data.title}</h5>
+                <p class="card-text">{data.desc}</p>
+                <p>{data.lastDonated}</p>
+                <hr style={cardLineStyle}/>
+                <p className="text-center">{data.totalDonation}</p>
+              </div>
+          </div>
+        </div>
+          )
+        })}
+      </div>
+    </div>
+    
+  )
+}
+
+export default TopFundraiserCard
