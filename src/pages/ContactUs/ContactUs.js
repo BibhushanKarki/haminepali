772aec97723cf5e151c4c4d1a01aece145a8d679
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-05-15 20:14:32 +0545

    hashtechnologies message

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const ContactUs = () => {
-  return (
-    <div>
-      <h1>Contact Us Page</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Contact Us Page</h1>
+		</div>
+	);
+};
 
-export default ContactUs
+export default ContactUs;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import TopFundraiserCard from '../../components/TopFundraiserCard/TopFundraiserCard'
-import VolunteerProfileCard from '../../components/VolunteerProfileCard/VolunteerProfileCard'
-import DownloadApp from '../../components/DownloadApp/DownloadApp'
-const Home = () => {
-  return (
-    <div>
-      <TopFundraiserCard/>
-      <VolunteerProfileCard/>
-      <DownloadApp/>
-    </div>
-  )
-}
+import React from 'react';
+import TopFundraiserCard from '../../components/TopFundraiserCard/TopFundraiserCard';
+import VolunteerProfileCard from '../../components/VolunteerProfileCard/VolunteerProfileCard';
+import DownloadApp from '../../components/DownloadApp/DownloadApp';
 
-export default Home
+const Home = () => {
+	return (
+		<div>
+			<TopFundraiserCard />
+			<VolunteerProfileCard />
+			<DownloadApp />
+		</div>
+	);
+};
+
+export default Home;
