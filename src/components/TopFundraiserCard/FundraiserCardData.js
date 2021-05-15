bea4320202beff2469c8ba9c4ddccc8ea5aa9bc7
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-05-15 12:23:27 +0545

    image url bug fix

--- src/components/TopFundraiserCard/FundraiserCardData.js
+++ src/components/TopFundraiserCard/FundraiserCardData.js
@@ -1,26 +1,29 @@
-export const FundraiserCardData=[
-  {
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  },
-  {
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  }
-  ,{
-    cardimg:require("../../assets/images/patient.jpg"),
-    location:'Biratnagar,Nepal',
-    title:'Help Saya Shakya Family',
-    desc:'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
-    lastDonated:'Last donated 10min Ago',
-    totalDonation:'Rs 10,000 raised of Rs 120,000'
-  }
-]
+export const FundraiserCardData = [
+	{
+		cardimg:
+			'https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9vciUyMHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		location: 'Biratnagar,Nepal',
+		title: 'Help Saya Shakya Family',
+		desc: 'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+		lastDonated: 'Last donated 10min Ago',
+		totalDonation: 'Rs 10,000 raised of Rs 120,000',
+	},
+	{
+		cardimg:
+			'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9vciUyMHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		location: 'Biratnagar,Nepal',
+		title: 'Help Saya Shakya Family',
+		desc: 'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+		lastDonated: 'Last donated 10min Ago',
+		totalDonation: 'Rs 10,000 raised of Rs 120,000',
+	},
+	{
+		cardimg:
+			'https://images.unsplash.com/photo-1527348696784-c3ff7c1453f9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9vciUyMHBlb3BsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+		location: 'Biratnagar,Nepal',
+		title: 'Help Saya Shakya Family',
+		desc: 'Saya Shakya (age 22)  is a cheerful,friendly person who lives in Biratnagar is suffering from',
+		lastDonated: 'Last donated 10min Ago',
+		totalDonation: 'Rs 10,000 raised of Rs 120,000',
+	},
+];
