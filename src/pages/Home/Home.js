36923f9c373d7aff3ae72efb23ec066602fc3f17
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-16 17:20:46 +0545

    after pull and dropdown routes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import TopFundraiserCard from '../../components/TopFundraiserCard/TopFundraiserCard';
-import VolunteerProfileCard from '../../components/VolunteerProfileCard/VolunteerProfileCard';
-import DownloadApp from '../../components/DownloadApp/DownloadApp';
-
-const Home = () => {
-	return (
-		<div>
-			<TopFundraiserCard />
-			<VolunteerProfileCard />
-			<DownloadApp />
-		</div>
-	);
-};
-=======
 import React from 'react'
 import HomePageCover from '../../components/HomePageCover/HomePageCover'
 import HomePageTexts from '../../components/HomePageTexts/HomePageTexts'
@@ -31,6 +15,5 @@ const Home = () => {
     </div>
   )
 }
->>>>>>> 15a3201e8d872d3171708c59bd9588b638c3f959
 
 export default Home;
