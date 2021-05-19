b790ef3db96387a738d143ada27e09668b30b572
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-19 16:24:45 +0545

    transparency

---
+++ src/components/TransparencyCard/TransparencyCard.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import AppButton from '../UI/Buttons/AppButton'
+import {TransparencyCardData as data} from './TransparencyCardData'
+
+const TransparencyCard = () => {
+  const cardStyle={
+    width: '18rem'
+  }
+  const ppStyle={
+    width:'100%',
+    objectFit:'cover',
+  }
+  const inputStyle={
+      height: '55px',
+     padding: '1rem',
+     fontSize:'20px',
+     border:'2px solid gray'
+  }
+  return (
+    <div className="TransparencyCardContainer my-4">
+      <div className="row d-flex justify-content-center my-4">
+        <div className="col-md-6">
+            <div class="input-group">
+              <div class="form-outline" style={{width:'100%'}}>
+                <input type="search" id="form1" class="form-control fw-bold" placeholder="Search Token" style={inputStyle}/>
+              </div>
+            </div>
+        </div>
+      </div>
+        
+      <div className="row">
+      <h2 className="fw-bold my-3 text-left" style={{color:'#9F1718',marginLeft:'2rem'}}>Ongoing Events</h2>
+      {data.map((data,key)=>{
+        return(
+          <div className="col-md-3 mb-3 d-flex justify-content-center" key={key}>
+              <div className="card" style={cardStyle}>
+                <img src={data.img} className="mb-3" style={ppStyle} alt="pp"/>
+                <div className="card-body text-left">
+                <h5 className="card-title fw-bold">{data.eventTitle}</h5>
+                  <p className="card-text">
+                    {data.eventDetail}
+                  </p>
+                </div>
+              </div>
+          </div>
+        )
+      })}
+      </div>
+      <div className="row">
+      <h2 className="fw-bold my-3 text-left" style={{color:'#9F1718',marginLeft:'2rem'}}>Ongoing Cause</h2>
+      {data.map((data,key)=>{
+        return(
+          <div className="col-md-3 mb-3 d-flex justify-content-center" key={key}>
+              <div className="card" style={cardStyle}>
+                <img src={data.img} className="mb-3" style={ppStyle} alt="pp"/>
+                <div className="card-body text-left">
+                <h5 className="card-title fw-bold">{data.eventTitle}</h5>
+                  <p className="card-text">
+                    {data.eventDetail}
+                  </p>
+                </div>
+              </div>
+          </div>
+        )
+      })}
+      </div>
+      <div className="row">
+      <h2 className="fw-bold my-3 text-left" style={{color:'#9F1718',marginLeft:'2rem'}}>Accomplished Events</h2>
+      {data.map((data,key)=>{
+        return(
+          <div className="col-md-3 mb-3 d-flex justify-content-center" key={key}>
+              <div className="card" style={cardStyle}>
+                <img src={data.img} className="mb-3" style={ppStyle} alt="pp"/>
+                <div className="card-body text-left">
+                <h5 className="card-title fw-bold">{data.eventTitle}</h5>
+                  <p className="card-text">
+                    {data.eventDetail}
+                  </p>
+                </div>
+              </div>
+          </div>
+        )
+      })}
+      </div>
+      <div className="row">
+      <h2 className="fw-bold my-3 text-left" style={{color:'#9F1718',marginLeft:'2rem'}}>Accomplished Cause</h2>
+      {data.map((data,key)=>{
+        return(
+          <div className="col-md-3 mb-3 d-flex justify-content-center" key={key}>
+              <div className="card" style={cardStyle}>
+                <img src={data.img} className="mb-3" style={ppStyle} alt="pp"/>
+                <div className="card-body text-left">
+                <h5 className="card-title fw-bold">{data.eventTitle}</h5>
+                  <p className="card-text">
+                    {data.eventDetail}
+                  </p>
+                </div>
+              </div>
+          </div>
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+
+export default TransparencyCard
---
+++ src/components/TransparencyCard/TransparencyCardData.js
@@ -0,0 +1,21 @@
+export const TransparencyCardData= [{
+  img:'https://images.unsplash.com/photo-1612277795421-9bc7706a4a34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+  eventTitle:'Raising fund for covid patinet',
+  eventDetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit nunc viverra nibh mauris, arcu nunc. Augue mauris tempus eget purus id quis fusce. Dolor in sit magna a ultrices adipiscing suspendisse.'
+},
+{
+  img:'https://images.unsplash.com/photo-1612277795421-9bc7706a4a34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+  eventTitle:'Raising fund for covid patinet',
+  eventDetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit nunc viverra nibh mauris, arcu nunc. Augue mauris tempus eget purus id quis fusce. Dolor in sit magna a ultrices adipiscing suspendisse.'
+},
+{
+  img:'https://images.unsplash.com/photo-1612277795421-9bc7706a4a34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+  eventTitle:'Raising fund for covid patinet',
+  eventDetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit nunc viverra nibh mauris, arcu nunc. Augue mauris tempus eget purus id quis fusce. Dolor in sit magna a ultrices adipiscing suspendisse.'
+},
+{
+  img:'https://images.unsplash.com/photo-1612277795421-9bc7706a4a34?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+  eventTitle:'Raising fund for covid patinet',
+  eventDetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit nunc viverra nibh mauris, arcu nunc. Augue mauris tempus eget purus id quis fusce. Dolor in sit magna a ultrices adipiscing suspendisse.'
+},
+]
