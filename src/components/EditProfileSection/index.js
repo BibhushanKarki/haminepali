5262663783617cc5d6ddb5905c43dcaee1c1cd1f
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-29 09:32:01 +0545

    edit icon added in edit profile page

--- src/components/EditProfileSection/index.js
+++ src/components/EditProfileSection/index.js
@@ -1,86 +1,105 @@
 import React from "react";
 import "./editprofile.css";
-
+import { FaRegEdit } from "react-icons/fa";
 const EditProfileSection = () => {
   return (
     <>
-      <div className="container mt-5">
-        <div className="row justify-content-center text-center">
-          <div className="col-md-12 ">
-            <div
-              className="card "
-              style={{ boxShadow: "none", border: "none" }}
-            >
-              <div
-                className="card-header"
-                style={{ backgroundColor: "#9f1718", height: 200 }}
-              ></div>
-              <div className="card-body text-center">
-                <img
-                  className="avatar rounded-circle"
-                  src="/img/pic2.jpg"
-                  alt="userProfile"
-                />
-                <h4 className="card-title">Levi Ackerman</h4>
+      <div
+        className="card w-50 mt-5  mx-auto "
+        style={{ boxShadow: "none", border: "none" }}
+      >
+        <div
+          className="card-header"
+          style={{ backgroundColor: "#9f1718", height: 200 }}
+        ></div>
+        <div className="card-body text-center">
+          <img
+            className="avatar rounded-circle"
+            src="/img/pic2.jpg"
+            alt="userProfile"
+          />
 
-                <form>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Firstname"
-                        name="firstname"
-                        required
-                      />
-                    </div>
-                    <div className="col-md-6">
-                      <input
-                        type="text"
-                        className="form-control lastname"
-                        placeholder="LastName"
-                        name="lastname"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Email-Address"
-                        name="email"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-3">
-                    <div className="col-md-12">
-                      <input
-                        type="password"
-                        className="form-control"
-                        placeholder="Password"
-                        name="password"
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="row mt-4 mb-3">
-                    <div className="col-md-12">
-                      <button type="submit" className=" btn btn-primary">
-                        Update
-                      </button>
-                      &nbsp;&nbsp;
-                      <button type="submit" className=" btn button-secondary">
-                        Save
-                      </button>
-                    </div>
-                  </div>
-                </form>
+          <form className="mt-4">
+            <div className="row">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Firstname"
+                  name="firstname"
+                  required
+                />
+                <span className="spanicon">
+                  <FaRegEdit />
+                </span>
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control lastname"
+                  placeholder="LastName"
+                  name="lastname"
+                  required
+                />
+                <span className="spanicon">
+                  <FaRegEdit />
+                </span>
               </div>
             </div>
-          </div>
+            <div className="row mt-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email-Address"
+                  name="email"
+                  required
+                />
+                <span className="spanicon">
+                  <FaRegEdit />
+                </span>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-12">
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Phone Number"
+                  name="phone"
+                  required
+                />
+                <span className="spanicon">
+                  <FaRegEdit />
+                </span>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-12">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  name="password"
+                  required
+                />
+                <span className="spanicon">
+                  <FaRegEdit />
+                </span>
+              </div>
+            </div>
+            <div className="row mt-4 mb-3">
+              <div className="col-md-12">
+                <button type="submit" className=" btn btn-primary">
+                  Save
+                </button>
+                &nbsp;&nbsp;
+                <button type="submit" className=" btn btn-primary">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </>
