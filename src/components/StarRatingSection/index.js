62c1306df16e40877fd70334f6845b2d1d8db7b9
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-29 21:02:02 +0545

    Star rating page done and added in volunteer-profile page

---
+++ src/components/StarRatingSection/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./starrating.css";
+
+const StarRatingSection = () => {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              className="star"
+              color={ratingValue <= (hover || rating) ? "#9F1718" : "#e4e5e9"}
+              size={30}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+      {/* <p style={{ color: "black" }}>The rating is {rating}.</p> */}
+    </div>
+  );
+};
+
+export default StarRatingSection;
