f27d1415b007e69de5da826230598235cb7e8bde
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-30 22:41:40 +0545

    added media queries in getstartedcovid page

--- src/components/FundraiserDescription/FundraiserDescription.js
+++ src/components/FundraiserDescription/FundraiserDescription.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import FundraiserDescriptionCard from "./FundraiserDescriptionCard";
 import { FundraiserDescriptionData as data } from "./FundraiserDescriptionData";
 import { Link } from "react-router-dom";
 import Comments from "./Comments";
 import AppButton from "../UI/Buttons/AppButton";
-=======
-import React from 'react'
-import FundraiserDescriptionCard from './FundraiserDescriptionCard'
-import Comments from './Comments'
-import {FundraiserDescriptionData as data} from './FundraiserDescriptionData'
-import AppButton from '../UI/Buttons/AppButton'
-import {Link} from 'react-router-dom'
->>>>>>> d2f3558f30379983ebf6192ee836827181121f02
 
 const FundraiserDescription = () => {
   return (
@@ -64,13 +55,8 @@ const FundraiserDescription = () => {
           </Link>
         </div>
       </div>
-<<<<<<< HEAD
     </div>
   );
 };
 
-=======
-    </div>)
-}
->>>>>>> d2f3558f30379983ebf6192ee836827181121f02
 export default FundraiserDescription;
