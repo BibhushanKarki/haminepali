a7f6a59c59621912ae8e708ef269bc5714ec8ae7
Bibhushan Karki <bibhushan.karki11@gmail.com>
2021-05-31 01:33:05 +0545

    top volunteers

--- src/components/FundraiserDescription/FundraiserDescriptionCard.js
+++ src/components/FundraiserDescription/FundraiserDescriptionCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {FundraiserDescriptionCardData as data} from './FundraiserDescriptionData'
+import {DonationData as item} from './FundraiserDescriptionData'
 import Fade from 'react-reveal/Fade';
 
 const FundraiserDescriptionCard = () => {
@@ -19,15 +20,15 @@ const FundraiserDescriptionCard = () => {
           <button className="btn btn-primary my-2 w-50 fw-bold" style={{fontSize:'22px'}}>Donate</button>
         </div>
         <p className="mt-4 hrblack">{data.recentDonation} people just donated</p>
-        {data.map((data,key)=>{
+        {item.map((item,key)=>{
           return( 
             <div className="d-flex mx-4 mb-3" key={key}>
               <img style={{height:'28px',width:'28px'}} src="https://image.flaticon.com/icons/png/512/860/860784.png" alt="img" />
               <div className="d-flex flex-column w-100 hrblack mx-3">
-                <p className="fw-bold text-start mx-2">{data.recentDoner}</p>
+                <p className="fw-bold text-start mx-2">{item.recentDoner}</p>
                 <div className="d-flex justify-content-between mx-2">
-                  <p>$ {data.recentDonerAmount}</p>
-                  <p>{data.donationStatus}</p>
+                  <p>$ {item.recentDonerAmount}</p>
+                  <p>{item.donationStatus}</p>
                 </div>
               </div>
             </div>
