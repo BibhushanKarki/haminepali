82d81eea3e8f96258dd3769a953de024fbff4131
Bibhushan <bibhushan.karki11@gmail.com>
2021-05-30 17:38:51 +0545

    milestones updated in about page and responsive done

--- src/components/AboutPageBanner/index.js
+++ src/components/AboutPageBanner/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AboutPageData as data } from "./AboutPageTextData/AboutPageData";
-
+import './timeline.css'
 const AboutPageBanner = () => {
   const aboutBanner = {
     marginTop: "3.5rem",
@@ -61,6 +61,70 @@ const AboutPageBanner = () => {
           </div>
         </div>
       </section>
+    <div className="container">  
+    <div className="page-header mb-4">
+        <h1 className="text-center" id="timeline"><u>Milestones</u></h1>
+    </div>
+    <ul className="timeline">
+        <li>
+          <div className="timeline-badge info"><i className="fa fa-check-square-o"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">After Earthquake</h4>
+            </div>
+            <div className="timeline-body">
+              <p>Shortly after, while the nation hadn’t even completely recovered from earthquake situation, Nepal faced another catastrophe. India blocked its borders with Nepal causing shortage of petroleum, LPG and rations. HamiNepal again worked together at such time to serve people.</p>
+              <hr/>
+            </div>
+          </div>
+        </li>
+        <li className="timeline-inverted">
+          <div className="timeline-badge success"><i className="fa fa-thumbs-o-up"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Mental Health Campaigns</h4>
+            </div>
+            <div className="timeline-body">
+              <p>Provided mental health campaigns in a bid to aware people about mental health.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="timeline-badge"><i className="fa fa-check-square-o"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Earthquake</h4>
+              <p><small className="text-muted"><i className="fa fa-clock-o"></i> 1 year ago</small></p>
+            </div>
+            <div className="timeline-body">
+              <p>Made houses for those who lost their properties due to the earthquake in several parts of  Nepal.</p>
+            </div>
+          </div>
+        </li>
+        <li className="timeline-inverted">
+          <div className="timeline-badge warning"><i className="fa fa-check-square-o"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Donations Received</h4>
+            </div>
+            <div className="timeline-body">
+              <p>Received donations from helpful donors.</p>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="timeline-badge danger"><i className="fa fa-check-square-o"></i></div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">Food Distribution</h4>
+            </div>
+            <div className="timeline-body">
+              <p>Provided food and clothes to needy people.</p>
+            </div>
+          </div>
+        </li>
+      </ul>    
+      </div>  
     </>
   );
 };
