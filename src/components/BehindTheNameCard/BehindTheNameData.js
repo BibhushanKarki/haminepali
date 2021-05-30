688608773a5e58540d9757b2ac9963f78d822eba
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-30 23:33:22 +0545

    some changes were done in behind the name page

--- src/components/BehindTheNameCard/BehindTheNameData.js
+++ src/components/BehindTheNameCard/BehindTheNameData.js
@@ -1,49 +1,8 @@
-export const BehindTheNameData=[{
-  img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-  designation:'CEO',
-  fullname:'Sadan Gurung',
-  desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
+export const BehindTheNameData = [
   {
-    img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-    designation:'CEO',
-    fullname:'Sadan Gurung',
-    desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    },
-    {
-      img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-      designation:'CEO',
-      fullname:'Sadan Gurung',
-      desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      },
-     {
-      img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-      designation:'CEO',
-      fullname:'Sadan Gurung',
-      desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      },
-    {
-      img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-      designation:'CEO',
-      fullname:'Sadan Gurung',
-      desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      },
-    {
-      img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-      designation:'CEO',
-      fullname:'Sadan Gurung',
-      desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      },
-      {
-        img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-        designation:'CEO',
-        fullname:'Sadan Gurung',
-        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-        },
-      {
-        img:'https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg',
-        designation:'CEO',
-        fullname:'Sadan Gurung',
-        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-        },
-]
+    img: "https://static.zerochan.net/Natsu.Dragneel.full.2085058.jpg",
+    designation: "CEO",
+    fullname: "Sadan Gurung",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis erat ut senectus tortor scelerisque egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
