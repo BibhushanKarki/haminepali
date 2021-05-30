291dbb3fabca7c0a2c7c08b4ed3e97dc737f8af2
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-30 16:22:15 +0545

    contents added in homepage

--- src/components/HomePageTexts/HomePageTextData.js
+++ src/components/HomePageTexts/HomePageTextData.js
@@ -1,11 +1,19 @@
-export const HomePageTextData=[
+export const HomePageTextData = [
   {
-    para:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Semper turpis morbi felis aliquam lacus, enim amet proin. Imperdiet amet, arcu, nulla eget sed ac. Lectus nec vitae lorem sed. In fames habitant netus a pretium suspendisse fusce nisl. Diam tellus elementum ut quis tincidunt faucibus sem penatibus. Vestibulum dui mauris molestie bibendum donec in. Sed mollis tristique magna viverra pellentesque amet, velit. Pellentesque amet, est turpis non vulputate turpis sagittis vulputate fringilla. Nunc sag ittis donec quam sed aliquetsitsc erisquelibero ullamcorper. Molestie lectus porttitor ut felis. Senectus at pellentesque mattis ru trum fames facilisis in urna accumsan. Vitae, enim sit sagittis, lectus sed. Nisi, tempor, lorem lectus enim,risus. Risus, mauris, volutpat amet lorem donec."
+    para: "Hami Nepal Youth Organisation is a non-profit organisation, established in 2015 A.D. and registered in 2020 A.D. (Regd.no. 609789065), which directly connects the donors and the recipients.",
   },
   {
-    para:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Semper turpis morbi felis aliquam lacus, enim amet proin. Imperdiet amet, arcu, nulla eget sed ac. Lectus nec vitae lorem sed. In fames habitant netus a pretium suspendisse fusce nisl. Diam tellus elementum ut quis tincidunt faucibus sem penatibus. "
+    para: "Hami Nepal has a track record of producing tangible and effective assistance during the public crisis in Nepal through volunteer work and donations; our efforts and assistance were widely lauded during the crisis arising in the aftermath of the earthquake in 2015. On 25 April, 2015 an earthquake with a magnitude of 7.8 Richter scale struck Nepal along with continued aftershocks occurring throughout the nation at the intervals of 15–20 minutes. The earthquake affected a huge population of the nation with about 9,000 fatalities and nearly 22,000 injured people. ",
   },
   {
-    para:"Vestibulum dui mauris molestie bibendum donec in. Sed mollis tristique magna viverra pellentesque amet, velit. Pellentesque amet, est turpis non vulputate turpis sagittis vulputate fringilla. Nunc sagittis donec quam sed aliquetsitsc erisquelibero ullam corper. Molestie lectus porttitor ut felis. Senectus at pellentesque mattis rutrum fames facilisis in urna accumsan. Vitae, enim sit sagittis, lectus sed. Nisi, tempor, lorem lectus enim,risus. Risus, mauris, volutpat amet lorem donec."
-  }
-]
+    para: "Now, the coronavirus pandemic has affected every nation in the world - some more than others. The COVID-19 cases have surged in Nepal to such an extent that the healthcare system of the nation is failing. The situation is dire with inadequate bed capacity, critical oxygen and medical supply shortages ultimately leading to personal loss of close-ones.",
+  },
+
+  {
+    para: "Amid this humanitarian crisis, Hami Nepal members and volunteers worked tirelessly to set up the COVID ward in Bir Hospital and National Trauma Center. In order to further ramp up health infrastructure, we have converted unused government spaces into COVID facilities and provided support to 3 different government hospitals. Hami Nepal has successfully set up 170 beds in Bir Hospital and donated 50 beds to the new building of Bir Hospital. The team has also donated high function ICU beds, portable X-ray machines among other medical supplies and equipment using the collected donations.",
+  },
+
+  {
+    para: "Our team recently received 520 oxygen cylinders from our donor Shankar group and Infinity holdings. We are also working our way to receive a portable high tech medical oxygen plant from the same donor. We have also deployed 100 beds and mattresses, 270 oxygen cylinders out of 520 and  medical supplies to the Sudurpaschim Province of Nepal and other places. Our donors provided us with 50,000 N95 Masks, 200 sets of PPE, 100 beds and other supplies which were then sent as aid to Sudurpaschim Province.",
+  },
+];
