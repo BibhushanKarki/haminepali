3355df5d9d113e33eda7752a80ca9f0445daec27
Bibhushan Karki <bibhushan.karki11@gmail.com>
2021-05-31 22:23:59 +0545

    alignment of paragraphs

--- src/components/HomePageTexts/HomePageTexts.js
+++ src/components/HomePageTexts/HomePageTexts.js
@@ -3,18 +3,18 @@ import { HomePageTextData as text } from './HomePageTextData';
 
 const HomePageTexts = () => {
 	return (
-		<div className='row my-5'>
-			<div className='col-md-12'>
-				{text.map((text, key) => {
-					return (
-						<p className='my-4 mx-5' key={key}>
-							{text.para}
-						</p>
-					);
-				})}
-			</div>
-		</div>
-	);
+    <div className="row my-5">
+      <div className="col-md-12 intro">
+        {text.map((text, key) => {
+          return (
+            <p className="my-4 mx-5" key={key}>
+              {text.para}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default HomePageTexts;
