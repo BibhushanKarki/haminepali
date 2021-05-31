7f3dc583f124bdea1429e1c48e4e3c16cc400d14
Sushma Chaudhary <susmachaudhary@gmail.com>
2021-05-31 16:54:37 +0545

    thumbnail image added in event page

--- src/components/TransparencyCard/TransparencyCardData.js
+++ src/components/TransparencyCard/TransparencyCardData.js
@@ -1,11 +1,14 @@
-export const TransparencyCardData= [{
-  img:'https://www.mumbailive.com/images/media/images/oxygen_plants_1619086394870.jpg?bg=dbdbd8&crop=1200%2C673.6842105263157%2C0%2Cnull&fit=crop&fitToScale=w%2C1368%2C768&h=606.3157894736842&height=675&w=1080&width=1200',
-  eventTitle:'Importing An Oxygen Plant',
-  eventDetail:'Our team has already finalised to import an oxygen plant and we are conducting thorough research on which hospital will be hugely benefited with the provided oxygen plant. The research is conducted so that the Hami Nepal organisation could identify which hospital will make better use of the equipment without any profit-making.By doing this, we believe that most of the COVID-19 infected people can be treated in their home and severe people will easily find beds in hospitals. Also, hospitals will not need to worry about lack of oxygen for patients. '
-},
-{
-  img:'https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2021/05/o2-concentrator-1200-1620293617.jpg',
-  eventTitle:'Importing An Oxygen Concentrator',
-  eventDetail:'We are collecting quality oxygen concentrators in order to build an oxygen concentrator bank which will directly benefit COVID-19 infected people. Hami Nepal will, however, conduct a need assessment so that we can verify the person is in serious need of oxygen concentrator and the equipment is not being misused.The people receiving the concentrators will also have to provide a photocopy of government issued Identity Card for proper tracking of equipment and to ensure the return-safety of equipment'
-}
-]
+export const TransparencyCardData = [
+  {
+    img: "https://www.mumbailive.com/images/media/images/oxygen_plants_1619086394870.jpg?bg=dbdbd8&crop=1200%2C673.6842105263157%2C0%2Cnull&fit=crop&fitToScale=w%2C1368%2C768&h=606.3157894736842&height=675&w=1080&width=1200",
+    eventTitle: "Importing An Oxygen Plant",
+    eventDetail:
+      "Our team has already finalised to import an oxygen plant and we are conducting thorough research on which hospital will be hugely benefited with the provided oxygen plant. The research is conducted so that the Hami Nepal organisation could identify which hospital will make better use of the equipment without any profit-making.By doing this, we believe that most of the COVID-19 infected people can be treated in their home and severe people will easily find beds in hospitals. Also, hospitals will not need to worry about lack of oxygen for patients. ",
+  },
+  {
+    img: "https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2021/05/o2-concentrator-1200-1620293617.jpg",
+    eventTitle: "Importing An Oxygen Concentrator",
+    eventDetail:
+      "We are collecting quality oxygen concentrators in order to build an oxygen concentrator bank which will directly benefit COVID-19 infected people. Hami Nepal will, however, conduct a need assessment so that we can verify the person is in serious need of oxygen concentrator and the equipment is not being misused.The people receiving the concentrators will also have to provide a photocopy of government issued Identity Card for proper tracking of equipment and to ensure the return-safety of equipment",
+  },
+];
